80e9b522574c9fab084e2423c13bd9e8ec0b81b9
vaseem15905 <138885639+vaseem15905@users.noreply.github.com>
2025-04-19 23:05:06 +0530

    initialized for deployment

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAjOOqBau6-GjeqYzzKXP61RJMlvGmVfDY",
-  authDomain: "tickytocky-todolist.firebaseapp.com",
-  projectId: "tickytocky-todolist",
-  storageBucket: "tickytocky-todolist.firebasestorage.app",
-  messagingSenderId: "989144636446",
-  appId: "1:989144636446:web:480795d0108080cd9ba20f"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
